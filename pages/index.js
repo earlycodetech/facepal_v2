a1cad63c635ea6f96b1e285a54b22deb585cb58e
Nwanne-san <nnamaninwanne@gmail.com>
2023-08-03 10:13:14 +0100

    edited image

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
   return ( 
     <>
       <div className="h-screen w-full flex flex-col justify-around bg">
-        <Image src="/imgs/bg-mobile.jpg" width={60} height={60} className="h-screen w-full sm:hidden" alt="cover image"/>
+        <Image src="/image/bg-mobile.jpg" width={60} height={60} className="h-screen w-full sm:hidden" alt="cover image"/>
         <div className="w-full flex flex-col sm:flex-row justify-center items-center gap-4 px-8 sm:px-10 lg:px-24">
             <div className="w-full sm:min-h-[480px] bg-indigo-300/20 py-6 sm:py-10 px-[40px] rounded-lg">
                 <h1 className="text-indigo-800 text-4xl sm:text-6xl font-mono font-bold">facepal {version}</h1>
