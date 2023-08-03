a1dec381928622afec2cc2893a001242be5def4f
unknown <clemnanzzy@gmail.com>
2023-08-03 12:48:56 +0100

    all work today

--- pages/account/make-transfer.js
+++ pages/account/make-transfer.js
@@ -1,10 +1,72 @@
-
+import { TextField,Box } from "@mui/material"
+import Link from "next/link"
+import { PastTransaction } from "../components/PastTransaction"
 export default function MakeTransfer() {
     return (
-        <main>
-            <h1 className="text-indigo-800 text-4xl sm:text-6xl font-mono font-bold">facepal</h1>
-            <h3 className="text-2xl mt-4">Make Transfer</h3>
+        <main className="px-6 sm:px-10 md:px-20">
+            <Link 
+            href="/feeds"
+            className="text-indigo-800 text-4xl sm:text-6xl font-mono font-bold">facepal</Link>
             
+           <section className=" grid md:grid-cols-2 gap-4 md:gap-6 mt-6">
+           <article className="bg-rose-100 p-4 rounded-xl">
+            <h3 className="text-2xl mt-4">Make Transfer</h3>
+
+            <form className="w-full flex flex-col gap-4 mt-4">
+                 <TextField id="outlined-basic" 
+                 label="account email"
+                  variant="outlined" 
+                  color="secondary"
+                  />
+
+                  <p>Jack pur</p>
+
+                  <TextField id="outlined-basic" 
+                 label="account email"
+                  variant="outlined" 
+                  color="secondary"/>
+
+                  <Button 
+                  variant="contained"
+                  color="secondary">Transfer
+                  </Button>
+            </form>
+            </article>
+
+            <aside className="border border-gray-200 p-4 rounded-xl">
+             <h3 className="text-3xl text-gray-700">Recent</h3>
+
+             <ul className=" h-[400px] flex flex-col gap-3">
+                <PastTransaction
+                recipient="Mary Adewale"
+                email="mary@interswitch.com"
+                amount={4600}
+                time={'13 hours'}
+                />
+                 <PastTransaction
+                recipient="Mary Adewale"
+                email="mary@interswitch.com"
+                amount={4600}
+                time={'13 hours'}
+                /> 
+                <PastTransaction
+                recipient="Mary Adewale"
+                email="mary@interswitch.com"
+                amount={4600}
+                time={'13 hours'}
+                /> 
+                <PastTransaction
+                recipient="Mary Adewale"
+                email="mary@interswitch.com"
+                amount={4600}
+                time={'13 hours'}
+                />
+                <Link href="/account/transfer-history"></Link>
+                    All Transfer history
+             </ul>
+            </aside>
+
+           </section>
         </main>
     )
 }
