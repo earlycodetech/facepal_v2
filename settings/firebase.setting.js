a06fe10e6dac3284bd36e432bf05120b547ee97b
Emmanuel Obiora <68205094+obiora198@users.noreply.github.com>
2023-08-15 11:05:07 +0100

    settings update

---
+++ settings/firebase.setting.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCbJ-tLudzjTT1Ngpvc1rZ_-ZyG5KK0ww0",
+  authDomain: "facepal-1bb1d.firebaseapp.com",
+  projectId: "facepal-1bb1d",
+  storageBucket: "facepal-1bb1d.appspot.com",
+  messagingSenderId: "791873849524",
+  appId: "1:791873849524:web:f666edfa92c1c86cb5afe1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const authentication = getAuth(app);
+
+export { authentication }
